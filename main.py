6eb5b0016b78f44188a5f3c72ce9cf17c014b203
guilherme <desaribeiro.guilherme@gmail.com>
2019-09-13 12:23:45 -0300

    Showing champions' names

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ def get_encrypted_summoner_id(summoner):
         raise Exception("Request error. Status code:", response.status_code)
 
 
+def get_champion_name(champions, champion_id):
+    for champion in champions["data"]:
+        if champions["data"][champion]["key"] == champion_id:
+            return champion
+
+
 def main():
     print("Verifique a sua maestria em League of Legends!")
     print()
@@ -26,9 +32,9 @@ def main():
     requests_cache.install_cache("cache")
 
     champions_response = requests.get("http://ddragon.leagueoflegends.com/cdn/9.18.1/data/pt_BR/champion.json")
+    champions = champions_response.json()
 
     summoner = input("Forneça o seu nome de invocador (servidor Brasileiro): ")
-
     encrypted_summoner_id = get_encrypted_summoner_id(summoner)
 
     mastery_response = requests.get("https://br1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-"
@@ -37,6 +43,9 @@ def main():
     mastery = mastery_response.json()
 
     top_three_mastery = mastery[:3]
+
+    for mastered in top_three_mastery:
+        print("%s: %d pontos" % (get_champion_name(champions, str(mastered["championId"])), mastered["championPoints"]))
 
 
 if __name__ == "__main__":
